5d5c6b5173980cab6f0020990578c160e62f0d11
wwaltsu <walter.ruoppa@metropolia.fi>
2022-02-25 00:57:28 +0200

    Refactored App.test code and ran npm update

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -7,44 +7,42 @@ import userEvent from "@testing-library/user-event"
 afterEach(cleanup)
 
 describe("<App />", () => {
-  test("On default all todos should be incomplete ", () => {
-    const component = render(<App />)
+  let container
 
-    const completeTodo = component.container.querySelectorAll(".todo-complete")
+  beforeEach(() => {
+    container = render(<App />).container
+  })
+  test("On default all todos should be incomplete ", () => {
+    const completeTodo = container.querySelectorAll(".todo-complete")
     expect(completeTodo.length).toBe(0)
   })
 
   test("Complete button should set complete to true", () => {
-    const component = render(<App />)
-
-    const button = component.container.querySelector("#complete-button")
+    const button = container.querySelector("#complete-button")
     userEvent.click(button)
-    const completeTodo = component.container.querySelectorAll(".todo-complete")
+    const completeTodo = container.querySelectorAll(".todo-complete")
     expect(completeTodo.length).toBe(1)
   })
 
   test("When input is empty it should NOT add new todo", () => {
-    const component = render(<App />)
-
-    const allTodos = component.container.querySelectorAll(".todo-incomplete")
-    const button = component.container.querySelector(".save-todo-btn")
+    const initialTodos = container.querySelectorAll(".todo-incomplete")
+    const button = container.querySelector(".save-todo-btn")
     userEvent.click(button)
-    const newAllTodos = component.container.querySelectorAll(".todo-incomplete")
-    expect(newAllTodos.length).toBe(allTodos.length)
+    const updatedTodos = container.querySelectorAll(".todo-incomplete")
+    expect(updatedTodos.length).toBe(initialTodos.length)
   })
 
   test("Adding a new todo", () => {
-    const component = render(<App />)
-    const input = component.container.querySelector(".input")
-    const allTodos = component.container.querySelectorAll(".todo-incomplete")
-    const button = component.container.querySelector(".save-todo-btn")
+    const input = container.querySelector(".input")
+    const initialTodos = container.querySelectorAll(".todo-incomplete")
+    const button = container.querySelector(".save-todo-btn")
 
     userEvent.type(input, "Take the dog out.")
     userEvent.click(button)
 
-    const newAllTodos = component.container.querySelectorAll(".todo-incomplete")
+    const updatedTodos = container.querySelectorAll(".todo-incomplete")
 
-    expect(newAllTodos.length).toBe(allTodos.length + 1)
-    expect(component.container).toHaveTextContent("Take the dog out.")
+    expect(updatedTodos.length).toBe(initialTodos.length + 1)
+    expect(container).toHaveTextContent("Take the dog out.")
   })
 })
